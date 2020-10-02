aa30ed0fa576f8fa08182ecec13c7dc93a238f0d
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-10-02 12:27:27 +0300

    Added more methods

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var app = document.querySelector("#app");
 var content =  document.querySelector("#content");
 
@@ -7,6 +9,7 @@ function toggleBodyAttrib(a) { if (ruler.getAttribute(a)=='1') ruler.setAttribut
 
 
 class MovableDiv {
+
 	constructor(width, height, background,  marginLeft="inherit") {
 		this.element = document.createElement('div');
 		this.element.classList.add("movable-div")
@@ -18,48 +21,64 @@ class MovableDiv {
 		this.element.style.margin = "20px"
 		this.element.style.marginLeft = marginLeft
 
-		this.element.addEventListener('click', this.log);
 		this.element.addEventListener('click', this.action);
 	}
 
-	log(){
-		console.log(
-			{
-				message: "Clicked!",
-				distanceTop: this.offsetTop + "px",
-				distanceLeft: this.offsetLeft + "px"
-			}
-		)
-	}
+
 
 	action(){
-		// Remove on all others
-		let elements = app.querySelectorAll("*");
 
-		if(this.style.outline){
-			this.style.outline = ""
+		const methods = {
+			log: () => {
+				console.log(
+					{
+						message: "Clicked!",
+						distanceTop: this.offsetTop + "px",
+						distanceLeft: this.offsetLeft + "px",
+						outline: this.style.outline
+					}
+				)
+		} ,
+			remove_outline_others: () => {
+				let elements = app.querySelectorAll("*");
+
+				for (let i=0, len = elements.length; i< len; i++){
+					if(elements[i].classList.contains("outline")){
+						elements[i].classList.remove("outline")
+					}
+				}
+			},
+			outline: () => {
+		
+				// this.style.outline = "2px solid #c3a9ff" ? "" : "2px solid #c3a9ff"
+
+				this.classList.toggle("outline")
+				// (this.style.outline) ? this.style.outline = "" : this.style.outline = "2px solid #c3a9ff" 
+				
+			},
 		}
 
-		for (let i=0, len = elements.length; i< len; i++){
-			if(elements[i].style.outline){
-				elements[i].style.outline = ""
-			}
-		}
-
-
-
-		this.style.outline = "2px solid green"
+		methods.log()
+		methods.remove_outline_others()
+		methods.outline()
+	
 	}
 
 	width() {
     return this.element.width
-  }
-
+	}
+	
 }
 
+
+
 const div1 = new MovableDiv(100, 50, "#7E7E7E");
-const div2 = new MovableDiv(200, 100, "#A5A5A5", marginLeft="150px");
+const div2 = new MovableDiv(200, 100, "#A5A5A5", "150px");
+
 
 content.appendChild(div1.element)
 content.appendChild(div2.element)
+
+
+
 
