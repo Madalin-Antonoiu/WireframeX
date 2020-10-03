bf4d3146222360e0c253f028c94cdbd95ba089c7
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-10-03 11:28:48 +0300

    full cycle: add, remove handles

--- script.js
+++ script.js
@@ -1,46 +1,112 @@
 "use strict"
+// Make Handle class instead of querySelectorAll
+
+//Thanks to https://gist.github.com/LeoAref/b6a14d96423a0b558b7de7635fb8f86e
+class Dispatcher {
+    constructor () {
+        this.events = {};   
+    }
+    
+    addListener (event, callback) {
+        // Check if the callback is not a function
+        if (typeof callback !== 'function') {
+            console.error(`The listener callback must be a function, the given type is ${typeof callback}`);
+            return false;
+        }
+        
+        
+        // Check if the event is not a string
+        if (typeof event !== 'string') {
+            console.error(`The event name must be a string, the given type is ${typeof event}`);
+            return false;
+        }
+        
+        // Check if this event not exists
+        if (this.events[event] === undefined) {
+            this.events[event] = {
+                listeners: []
+            }
+        }
+        
+        this.events[event].listeners.push(callback);
+    }
+    
+    removeListener (event, callback) {
+        // Check if this event not exists
+        if (this.events[event] === undefined) {
+            console.error(`This event: ${event} does not exist`);
+            return false;
+        }
+        
+    	this.events[event].listeners = this.events[event].listeners.filter(listener => {
+    	    return listener.toString() !== callback.toString(); 
+    	});
+    }
+    
+    dispatch (event, details) {
+        // Check if this event not exists
+        if (this.events[event] === undefined) {
+            console.error(`This event: ${event} does not exist`);
+            return false;
+        }
+        
+        this.events[event].listeners.forEach((listener) => {
+            listener(details);
+        });
+    }  
+}
+
 class MovableDiv {
 
-	constructor(width, height, background) {
-		this.element = document.createElement('div');
-		this.element.innerHTML= `
-		<span class="resizable-handle" style="bottom:0; right:0;" ></span>
-		<span class="resizable-handle" style="bottom:0; left:0;"></span>
-		<span class="resizable-handle" style="top:0; right:0;"></span>
-		<span class="resizable-handle" style="top:0; left:0;"></span>
-
-		<span class="resizable-handle" style="top:-2.5px; left:50%;"></span>
-		<span class="resizable-handle" style="bottom:-2.5px;; left:50%;"></span>
-
-		<span class="resizable-handle" style="top:50%; left:-2.5px;;"></span>
-		<span class="resizable-handle" style="top:50%; right:-2.5px;"></span>
-		`
-
-
-		this.element.classList.add("movable-div");
-
-		this.element.style.display = "inline-block";
-		this.element.style.width = width + "px";
-		this.element.style.height = height+ "px";
-		this.element.style.background = background;
-		this.element.style.margin = "0 20px 0 20px";
-		this.element.style.position="absolute";
+	constructor(w, h, bg) {
+		this.helpers.init(w,h,bg)
 
 		this.element.addEventListener('mousedown', this.methods.mousedown);
+		document.addEventListener('click', this.helpers.handles)
+
 	}
 
 
 	helpers = {
-		// log: () => {
-		// 	console.log(
-		// 		{
-		// 			message: "Clicked!",
-		// 			distanceTop: this.element.offsetTop + "px",
-		// 			distanceLeft: this.element.offsetLeft + "px",
-		// 			outline: this.element.style.outline
-		// 		}
-		// 	)
-		// } ,
+		init : (w,h,bg) =>{
+			this.element = document.createElement('div');
+			this.element.classList.add("movable-div");
+			this.element.style.display = "inline-block";
+			this.element.style.width = w + "px";
+			this.element.style.height = h+ "px";
+			this.element.style.background = bg;
+			this.element.style.margin = "0 20px 0 20px";
+			this.element.style.position="absolute";
+		},
+		handles : (event) => {
+			event.preventDefault();
+			event.stopPropagation();
+
+			// dispatcher.dispatch('delete-handles');
+
+			if(!this.element.querySelector(".resizable-handle")){
+				console.log("ADD handles")
+				this.vars.hasHandles = true;
+				this.helpers.attachHandles();
+			}
+
+			if (!this.element.contains(event.target)) {
+				console.log("REMOVE handles!")
+				this.helpers.removeOtherHandles()
+			  //the click was outside the specifiedElement, do something
+			}
+		},
+		attachHandles : () =>{
+
+			this.element.append(new Handle({bottom: "0px", left:"0px"}).element);
+			this.element.append(new Handle({bottom: "0px", right:"0px"}).element);
+			this.element.append(new Handle({top: "0px", left:"0px"}).element);
+			this.element.append(new Handle({top: "0px", right:"0px"}).element);
+			this.element.append(new Handle({top:"-2.5px", left:"50%"}).element);
+			this.element.append(new Handle({bottom:"-2.5px", left:"50%"}).element);
+			this.element.append(new Handle({top:"50%", left:"-2.5px"}).element);
+			this.element.append(new Handle({top:"50%", right:"-2.5px"}).element);
+		},
 		removeOutlineOnOthers: () => {
 			let elements = app.querySelectorAll("*");
 
@@ -68,6 +134,9 @@ class MovableDiv {
 		moveElem: (ev) =>{
 
 			if (this.vars.isDown == true) {
+
+
+
 				this.vars.mousePosition = {
 		
 					x : ev.clientX,
@@ -75,12 +144,22 @@ class MovableDiv {
 			
 				};
 
-				// console.log(this.vars.mousePosition);
+				// dispatcher.dispatch('hide-handles');
+
 				this.element.style.left = (this.vars.mousePosition.x + this.vars.offset[0]) + 'px';
 				this.element.style.top  = (this.vars.mousePosition.y + this.vars.offset[1]) + 'px';
 		
 			}
+		},
+		removeOtherHandles: () => {
+
+			// dispatcher.dispatch('delete-handles');
+			let handles = this.element.querySelectorAll(".resizable-handle");
+			for(let i=0, len = handles.length;i < len; i++){
+				handles[i].remove()
+			}
 		}
+
 	}
 
 	vars = {
@@ -90,7 +169,7 @@ class MovableDiv {
 
 		mousePosition: "",
 		offset: [0,0],
-		isDown: false
+		isDown: false,
 	}
 
 	methods = {
@@ -116,7 +195,7 @@ class MovableDiv {
 
 		},
 		mousedown : (ev) =>{
-
+			dispatcher.dispatch('hide-handles');
 			this.vars.isDown = true;
 	
 			this.vars.offset = [
@@ -141,9 +220,15 @@ class MovableDiv {
 			
 		},
 		mouseup: (ev) => {
+			ev.preventDefault();
+			ev.stopPropagation();
+
 			this.vars.isDown = false;
 			console.log("isDown", this.vars.isDown)
 			this.helpers.terminate()
+
+			dispatcher.dispatch('show-handles');
+		
 
 		},
 		mouseleave: (ev) => {
@@ -156,6 +241,37 @@ class MovableDiv {
 	}
 
 }
+
+class Handle {
+	constructor({left, top, bottom, right}){
+		this.element = document.createElement('span');
+		this.element.classList.add("resizable-handle");
+
+		this.element.style.left = left 
+		this.element.style.top = top
+		this.element.style.right = right 
+		this.element.style.bottom = bottom 
+
+		dispatcher.addListener('hide-handles', () => {
+			this.element.classList.add("hide")
+		});
+		dispatcher.addListener('show-handles', () => {
+			this.element.classList.remove("hide")
+		});
+
+		// dispatcher.addListener('delete-handles', () => {
+		// 	console.log("I should DELETE the handles") 
+		// 	this.element.remove()
+		// });
+	
+	}
+
+
+
+	//when moving, hide them!
+}
+
+const dispatcher = new Dispatcher();
 
 var app = document.querySelector("#app");
 var content =  document.querySelector("#content");
