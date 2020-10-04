45bdeb45dd1375cddde87ce898049bb1eec3549b
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-10-04 16:00:43 +0300

    dsadsad

--- classes/Container.js
+++ classes/Container.js
@@ -1,3 +1,6 @@
+import {bulma} from "../bulma-components.js"
+
+
 export default class Container {
 	constructor(){
 		this.elm = document.createElement("BODY");
@@ -6,34 +9,69 @@ export default class Container {
         this.elm.addEventListener("click", this.methods.click)
         this.elm.addEventListener("mouseover", this.methods.mouseover)
         this.elm.addEventListener("mouseout", this.methods.mouseout)
+        this.elm.addEventListener("contextmenu", this.methods.contextmenu)
+ 
     }
 
     data = {
         prevElm: null,
-        enableCall: true
+        enableCall: true,
     }
 
     methods={
         click : (ev) => {
             console.log("Clicked!", ev.target.nodeName)
-
-            
-           
-           
+            document.querySelector("#myModal").classList.remove("is-active");
         },
         mouseover : (ev) => {
+
+
             this.data.prevElm ? this.data.prevElm.classList.remove("outline")  : ""
             this.data.prevElm = ev.target;
             ev.target.classList.add("outline");
+
+            // Add badge
+            let badge = document.createElement("span"); badge.classList.add("badge");
+            badge.innerHTML = ev.target.nodeName
+
+
+            if(!ev.target.querySelector(".badge") && ev.target.id !== "badge"){
+                ev.target.appendChild(badge);
+            }
+           
             
         },
         mouseout :(ev) => {
             this.data.prevElm ? this.data.prevElm.classList.remove("outline")  : ""
+            this.data.prevElm ? this.data.prevElm.querySelector(".badge").remove()  : ""
+        },
+        contextmenu :(ev) => {
+            //here you draw your own menu
+            ev.preventDefault();
+
+            // let contextMenu = document.createElement("div"); contextMenu.id="contextMenu";
+            // contextMenu.innerHTML = bulma.modal;
+
+
+            let modal = document.querySelector("#myModal");
+
+            if(modal.classList.contains('is-active')){
+                modal.style.top      = ev.clientY + 'px';
+                modal.style.left     = ev.clientX + 'px';
+               
+            } else {
+                modal.classList.add("is-active")
+                modal.style.top      = ev.clientY + 'px';
+                modal.style.left     = ev.clientX + 'px';
+            }
+
+
         }
-
-
 			
 
 
     }
 }
+
+
+
--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 "use strict"
 import Container from "./classes/Container.js"
 
+var mdl = document.createElement("DIV");mdl.id="myModal"; mdl.classList.add("modal");
+mdl.innerHTML= `
+	
+	<div class="modal-card">
+		<header class="modal-card-head">
+			<p class="modal-card-title">Opts</p>
+			<button class="delete" aria-label="close" data-bulma-modal="close"></button>
+		</header>
+		<section class="modal-card-body">
+			<p>There is something here</p>
+		</section>
+		<footer class="modal-card-foot">
+			<button class="button is-success">Save changes</button>
+			<button class="button" data-bulma-modal="close">Cancel</button>
+		</footer>
+	</div>
+`
+
+
+mdl.querySelector("[data-bulma-modal='close']").addEventListener("click", function(){
+	mdl.classList.toggle('is-active')
+})
+
+
 var app = document.querySelector("#app");
 app.appendChild(new Container().elm);
 var content =  document.querySelector("#content");
 
+console.log(mdl)
+app.appendChild(mdl) 
 
 
-let jafa = document.createElement("DIV"); jafa.style.height="100px"; jafa.style.width="100px"; jafa.style.background = "#888888";
+
+
+let jafa = document.createElement("DIV"); jafa.style.height="100px"; jafa.style.width="100px"; jafa.style.background = "#888888"; jafa.style.position = "relative";
 content.appendChild(jafa)
+
+
+
